cda17e4f28c65336fad3d4ab8c60abbb6acd5add
sureshsarkar <sureshsarkar2020@gmail.com>
2024-08-29 14:52:49 +0530

    add Live ur nextl

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import Login from './pages/login/Login.jsx';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import './App.css'
 import SignUp from './pages/signup/SignUp.jsx';
 import Home from './pages/home/Home.jsx';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,9 +12,9 @@ function App() {
     <>
         <Router>
           <Routes>
-            <Route exact path="/" element={<Home/>}/>
-            <Route exact path="/login" element={<Login/>}/>
-            <Route exact path="/signup" element={<SignUp/>}/>
+            <Route  path="/" element={<Home/>}/>
+            <Route  path="/login" element={<Login/>}/>
+            <Route  path="/signup" element={<SignUp/>}/>
           </Routes>
         </Router>
     </>
