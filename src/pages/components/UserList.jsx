9d08d0a22e13985580381ee71b0b532d5112ad83
sureshsarkar <sureshsarkar2020@gmail.com>
2024-08-29 17:21:17 +0530

    login in page active change log last chages

--- src/pages/components/UserList.jsx
+++ src/pages/components/UserList.jsx
@@ -1,38 +1,39 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
+import axios from 'axios';
 
 const UserList = () => {
+const [users,setUsers] = useState([]);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null); 
+const allUser = async ()=>{
+ try {
+    const allusers = await axios.post(`https://chatappbackend-l6tu.onrender.com/api/auth/alluser`, null);
+    console.log(allusers);
+    setUsers(allusers.data)
+  } catch (error) {
+    setError(error.message);  // Handle any errors that occur during fetch
+  } finally {
+    setLoading(false);  // Set loading to false once the request is complete
+  }
+
+  }
+  useEffect(()=>{
+    allUser();
+  },[])
+  if(loading) return <p>Loading...</p>;  // Display loading state
+  if (error) return <p>Error: {error}</p>;  // Display error message if any
   return (
     <>
        <div className="scrollBar">
-          
+       
+          {users.map(user => (
           <div className="d-flex py-2 border-b-2">
               <div className="userImg">
                 <img src="../src/assets/images/home.jpg" alt="Logo" />
               </div>
               <div className="p-2 fullNameText">Suresh Sarkar</div>
           </div>
-
-          <div className="d-flex  py-2 border-b-2">
-              <div className="userImg">
-                <img src="../src/assets/images/home.jpg" alt="Logo" />
-              </div>
-              <div className="p-2 fullNameText">Suresh Sarkar</div>
-          </div>
-
-          <div className="d-flex  py-2 border-b-2">
-              <div className="userImg">
-                <img src="../src/assets/images/home.jpg" alt="Logo" />
-              </div>
-              <div className="p-2 fullNameText">Suresh Sarkar</div>
-          </div>
-
-          <div className="d-flex  py-2 border-b-2">
-              <div className="userImg">
-                <img src="../src/assets/images/home.jpg" alt="Logo" />
-              </div>
-              <div className="p-2 fullNameText">Suresh Sarkar</div>
-          </div>
-          
+          ))}
       </div>
     </>
   )
